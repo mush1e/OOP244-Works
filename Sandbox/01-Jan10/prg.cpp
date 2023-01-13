42cf43c40a9731fc736a174095c107e4031cde9d
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-01-13 12:08:35 -0500

    Required help! Bug in line 3

--- Sandbox/01-Jan10/prg.cpp
+++ Sandbox/01-Jan10/prg.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
    cout << "Hello oop244naa" << endl
-      << "Welcome to 2231 semester!" << endl;
+      << "Welcome to 2231 semester!" << endl
+      << "Appended content" << endl;
    return 0;
 }
