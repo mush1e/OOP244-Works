240cb8aa0b90744d5bb9e04c7c3e10a162c684c6
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-01-14 19:42:18 -0500

    Added week1 content

--- Sandbox/01-Jan10/prg.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-   cout << "Hello oop244naa" << endl
-      << "Welcome to 2231 semester!" << endl
-      << "Appended content" << endl;
-   return 0;
-}
