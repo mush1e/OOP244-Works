96b9ed045f9130235f4757737801d47d01be228c
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-01-16 13:48:34 -0500

    All features implemented except !

--- W01/DIY/UI.cpp
+++ W01/DIY/UI.cpp
@@ -8,19 +8,44 @@ namespace sdds {
     char op;
     double value;
 
-    void displayOutput(GPA* gpa) {
-        int length = recordLength();
-        cout.precision(2);
+    int displayOutput(GPA* gpa) {
+        char quitChar;
+        int length = recordLength(),
+            returnValue = 1;
+
+        cout.precision(1);
+
         for(int i = 0; i < length; i++) {
             switch(op) {
                 case '<':
                     if(gpa[i].gpa < value)
-                        cout << gpa[i].stno << ": " 
-                             << gpa[i].gpa  << " ("  
+                        cout << gpa[i].stno << ": " <<
+                             fixed << gpa[i].gpa  << " ("  
                              << gpa[i].name << ")" << endl;
                     break;
+
+                case '>':
+                    if(gpa[i].gpa > value)
+                        cout << gpa[i].stno << ": "  <<
+                             fixed << gpa[i].gpa  << " ("  
+                             << gpa[i].name << ")" << endl;
+                    break;
+
+                case '~':
+                    if(gpa[i].gpa > value-0.05 && gpa[i].gpa < value+0.05)
+                        cout << gpa[i].stno << ": " <<
+                             fixed << gpa[i].gpa  << " ("  
+                             << gpa[i].name << ")" << endl;
+                    break;
+                
+                default:
+                    cout << "Exit the program? (Y)es/(N)o: ";
+                    cin >> quitChar;
+                    if (quitChar == 'y')
+                        returnValue = 0;
             }
         }
+        return returnValue;
     }
 
     void promptUser(GPA* gpa) {
@@ -34,7 +59,7 @@ namespace sdds {
                      << "Op: [>,<,~,!]" << endl;
                 continue;
             }
-            displayOutput(gpa);
+            flag = displayOutput(gpa);
         }
     }
 }
