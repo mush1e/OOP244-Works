65a0aa6851a103f31cf4d76a3c1894dae561a53e
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-01-16 12:19:31 -0500

    Added GPA based sorting

--- W01/DIY/File.cpp
+++ W01/DIY/File.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 namespace sdds {
 
-    GPA gpaList[100];
     int length;
 
     GPA* readRecords(FILE* fptr) {
-
-        for(    length = 0
-            ;   fscanf(fptr, "%[^,],%d,%lf\n",   gpaList[length].name, 
-                                                &gpaList[length].stno, 
-                                                &gpaList[length].gpa)==3
-            ;   length++); 
+        GPA gpaList[100];
+        
+        for(length = 0
+           ;fscanf(fptr, "%[^,],%d,%lf\n",   gpaList[length].name, 
+                                            &gpaList[length].stno, 
+                                            &gpaList[length].gpa)==3
+           ;length++); 
         
         return gpaList;
     }
     
     int recordLength() {
-        return sdds::length;
+        return length;
     }
     
 
--- W01/DIY/GPAlist.cpp
+++ W01/DIY/GPAlist.cpp
@@ -9,12 +9,21 @@ using namespace std;
 namespace sdds {
 
     FILE* fptr;
-    GPA* gpa;
+    GPA* gpaList;
+
+    void sort(int length) {
+        for(int i = 0; i < length - 1; i++) {
+            for(int j = 0; j < length - i - 1; j++) {
+                if(gpaList[j].gpa < gpaList[j+1].gpa)
+                    swap(gpaList[j], gpaList[j+1]);
+            }
+        }
+    }
 
     bool gpaQuery(const char* filename) {
         fptr = fopen(filename, "r");
         if(fptr) {
-            gpa = readRecords(fptr);
+            gpaList = readRecords(fptr);
             int length  = recordLength();
             return true;
         }
