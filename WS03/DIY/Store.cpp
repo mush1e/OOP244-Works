8d7cba264b58f0ac0be87fae235cc23a5a215c75
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-02-08 23:17:51 -0500

    added declaration and reflect.txt

--- WS03/DIY/Store.cpp
+++ WS03/DIY/Store.cpp
@@ -1,3 +1,12 @@
+/***********************************************************************
+                 Store.cpp
+ Name: Mustafa Siddiqui
+ Email: msiddiqui48@myseneca.ca
+ Student Id: 117424225
+ Date: 02/8/23
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+***************************************************************************/
 #include <iostream>
 #include <cstring>
 #include <iomanip>
--- WS03/DIY/Store.h
+++ WS03/DIY/Store.h
@@ -1,3 +1,12 @@
+/***********************************************************************
+                 Store.h
+ Name: Mustafa Siddiqui
+ Email: msiddiqui48@myseneca.ca
+ Student Id: 117424225
+ Date: 02/8/23
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+***************************************************************************/
 #ifndef SDDS_STORE_H
 #define SDDS_STORE_H
 
