a1ba6a807e6cc4228c04cc84f89c609eeb075120
Mustafa Siddiqui <mu5h1e@Mustafas-MacBook-Pro.local>
2023-03-23 00:12:29 -0400

    Fixed code

--- MS2/Error.cpp
+++ MS2/Error.cpp
@@ -39,10 +39,8 @@ namespace sdds {
     }
 
     Error::~Error() {
-       if (*this) {
           delete[] this->m_ErrMsgPtr;
           this->m_ErrMsgPtr = nullptr;
-       }
     }
 
     Error& Error::operator=(const Error& other) {
