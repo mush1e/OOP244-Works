c0b599305d3378f1b9f3236cb97947e596073c70
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-04-06 04:23:04 -0400

    Code works but I has bug

--- MS4/Perishable.cpp
+++ MS4/Perishable.cpp
@@ -32,7 +32,28 @@ namespace sdds {
                 : ostr << "Expiry date: " << this->m_expiry
                        << endl << "=============^" << endl);
         }
+        return ostr;
     }
 
+    ifstream& Perishable::load(ifstream& istr) {
+        Item::load(istr);
+        char ch;
+        if(*this && !istr.fail()) {
+            Date expDate {};
+            expDate.dateOnly(true);
+            istr >> ch >> expDate;
+            expDate.validateDate() 
+                ? this->m_expiry = expDate 
+                : this->m_expiry, 
+                  this->err = expDate.error();
+        }
+        return istr;
+    }
+
+    ofstream& Perishable::save(ofstream& ostr) const {
+        Item::save(ostr);
+        *this && ostr << "," << this->m_expiry;
+        return ostr;
+    }
 
 }
