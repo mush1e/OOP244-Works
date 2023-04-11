908ef92a029e35f9339184237ec0395f63a1997b
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-04-11 17:47:06 -0400

    MS51

--- MS5/main.cpp
+++ MS5/main.cpp
@@ -1,40 +1,16 @@
-// Final Project
-// Point of Sale Application
+// Final Project Milestone 2
+// Student POS App Mockup tester
 // Version 1.0
-// Date  2023-03-29
-// Author   Fardad Soleimanloo
+// Date	2023-02-28
+// Author	Fardad Soleimanloo
 // Description
-// This program test the student implementation of the Point of
-// Sale Application
+// This program test the student implementation of the PosApp class
+// for submission.
 //
 /////////////////////////////////////////////////////////////////
-#include <fstream>
-#include <iostream>
 #include "PosApp.h"
-void resetDataFile();
-void displayfileContent();
 int main() {
-   resetDataFile();
-   displayfileContent();
-   sdds::PosApp pos("posdata.csv");
+   sdds::PosApp pos("datafile.csv");
    pos.run();
-   displayfileContent();
    return 0;
-}
-void resetDataFile() {
-   char ch{};
-   std::ofstream outfile("posdata.csv");
-   std::ifstream infile("posdataOrigin.csv");
-   while(infile.get(ch)) {
-      outfile.put(ch);
-   }
-}
-void displayfileContent() {
-   std::cout << "Data file:\n------------------\n";
-   char ch{};
-   std::ifstream infile("posdata.csv");
-   while(infile.get(ch)) {
-      std::cout.put(ch);
-   }
-   std::cout << "------------------\n";
 }
