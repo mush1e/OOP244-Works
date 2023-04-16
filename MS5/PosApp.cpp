ac3b0010c313ad4578309a0268ca9e37e0931f72
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-04-16 06:23:13 -0400

    DMA

--- MS5/PosApp.cpp
+++ MS5/PosApp.cpp
@@ -28,7 +28,8 @@ using namespace std;
 namespace sdds {
 
     PosApp::~PosApp() {
-        for(int i = 0; i < this->m_nptr && this->m_Iptr[i]->clear(); i++);
+        for(int i = 0; i < this->m_nptr; i++)
+            this->m_Iptr[i]->clear();
     }
 
     PosApp::PosApp(const char* fileName) {
@@ -74,7 +75,7 @@ namespace sdds {
             m_Iptr[i] = iptr;
             m_Iptr[i]->load(ifstr);
             m_Iptr[i]->displayType(POS_LIST);
-            delete iptr; 
+            // delete iptr; 
         }
         this->m_nptr = i-1;
         return ostr;
